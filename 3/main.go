aa5fdfeba21abd3f587ef3bd0c1130dd571e4a61
Dmitry Verkhoturov <paskal.07@gmail.com>
2023-12-02 21:20:10 +0100

    add file reading to the boilerplate

--- 3/main.go
+++ 3/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	_ "embed"
+	"log"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
 func main() {
-	
+	for i, s := range strings.Split(input, "\n") {
+		log.Printf("line %d: %q", i+1, s)
+	}
 }
