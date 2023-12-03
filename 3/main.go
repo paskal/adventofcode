d88b2683fcb4e02d9c6e05a85667b224095d3ea6
Dmitry Verkhoturov <paskal.07@gmail.com>
2023-12-03 22:38:52 +0100

    day 3 part 2 solution

--- 3/main.go
+++ 3/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
 	"log"
 	"regexp"
 	"strconv"
@@ -22,37 +23,59 @@ func main() {
 			lines[x][y] = c
 		}
 	}
+	var gears = map[string][]int{}
 	isDigit := regexp.MustCompile(`\d`)
 	for y, line := range lines {
 		var currentNumber string
 		var numberHasAdjustedSymbol []int
+		var symbol string
+		var adjustedToGear bool
 		for x, c := range line {
 			if c != emptyLine && isDigit.MatchString(c) {
 				currentNumber += c
 				if numberHasAdjustedSymbol == nil {
-					numberHasAdjustedSymbol = hasAdjustedSymbols(lines, x, y)
+					numberHasAdjustedSymbol, symbol = hasAdjustedSymbols(lines, x, y)
+					if symbol == "*" {
+						adjustedToGear = true
+					}
 				}
 			}
 			if !isDigit.MatchString(c) && currentNumber != "" {
 				if numberHasAdjustedSymbol != nil {
 					digit, _ := strconv.Atoi(currentNumber)
 					sum += digit
+					if adjustedToGear {
+						gearCoordinates := fmt.Sprintf("%d%d", numberHasAdjustedSymbol[0], numberHasAdjustedSymbol[1])
+						gears[gearCoordinates] = append(gears[gearCoordinates], digit)
+					}
 				}
 				currentNumber = ""
 				numberHasAdjustedSymbol = nil
+				adjustedToGear = false
 			}
 		}
 		if numberHasAdjustedSymbol != nil && currentNumber != "" {
 			digit, _ := strconv.Atoi(currentNumber)
 			sum += digit
+			if adjustedToGear {
+				gearCoordinates := fmt.Sprintf("%d%d", numberHasAdjustedSymbol[0], numberHasAdjustedSymbol[1])
+				gears[gearCoordinates] = append(gears[gearCoordinates], digit)
+			}
 		}
 		currentNumber = ""
 		numberHasAdjustedSymbol = nil
+		adjustedToGear = false
 	}
-	log.Printf("Sum of all detail parts: %d", sum)
+	var sumGears int
+	for _, parts := range gears {
+		if len(parts) == 2 {
+			sumGears += parts[0] * parts[1]
+		}
+	}
+	log.Printf("Sum of all detail parts: %d, sum of gear and multiplications: %d", sum, sumGears)
 }
 
-func hasAdjustedSymbols(lines [][]string, x int, y int) []int {
+func hasAdjustedSymbols(lines [][]string, x int, y int) (xy []int, symbol string) {
 	isNotDigitOrPoint := regexp.MustCompile(`[^\d.]`)
 	notTopLine := x > 0
 	notLastLine := x < len(lines[y])-1
@@ -61,44 +84,44 @@ func hasAdjustedSymbols(lines [][]string, x int, y int) []int {
 	if notFirstColumn {
 		if notTopLine {
 			if isNotDigitOrPoint.MatchString(lines[y-1][x-1]) {
-				return []int{x - 1, y - 1}
+				return []int{x - 1, y - 1}, lines[y-1][x-1]
 			}
 		}
 		if isNotDigitOrPoint.MatchString(lines[y-1][x]) {
-			return []int{x, y - 1}
+			return []int{x, y - 1}, lines[y-1][x]
 		}
 		if notLastLine {
 			if isNotDigitOrPoint.MatchString(lines[y-1][x+1]) {
-				return []int{x + 1, y - 1}
+				return []int{x + 1, y - 1}, lines[y-1][x+1]
 			}
 		}
 	}
 
 	if notTopLine {
 		if isNotDigitOrPoint.MatchString(lines[y][x-1]) {
-			return []int{x - 1, y}
+			return []int{x - 1, y}, lines[y][x-1]
 		}
 	}
 	if notLastLine {
 		if isNotDigitOrPoint.MatchString(lines[y][x+1]) {
-			return []int{x + 1, y}
+			return []int{x + 1, y}, lines[y][x+1]
 		}
 	}
 
 	if notLastColumn {
 		if notTopLine {
 			if isNotDigitOrPoint.MatchString(lines[y+1][x-1]) {
-				return []int{x - 1, y + 1}
+				return []int{x - 1, y + 1}, lines[y+1][x-1]
 			}
 		}
 		if isNotDigitOrPoint.MatchString(lines[y+1][x]) {
-			return []int{x, y + 1}
+			return []int{x, y + 1}, lines[y+1][x]
 		}
 		if notLastLine {
 			if isNotDigitOrPoint.MatchString(lines[y+1][x+1]) {
-				return []int{x + 1, y + 1}
+				return []int{x + 1, y + 1}, lines[y+1][x+1]
 			}
 		}
 	}
-	return nil
+	return nil, ""
 }
