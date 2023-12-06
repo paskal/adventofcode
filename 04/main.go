7fd13e563f6bd9783df4a8d4f7bd5a7dd68e5e9c
Dmitry Verkhoturov <paskal.07@gmail.com>
2023-12-06 01:39:38 +0100

    hack to simplify code

--- 04/main.go
+++ 04/main.go
@@ -32,7 +32,8 @@ func main() {
 		var winningNumbers = map[int]struct{}{}
 		var currentNumber string
 		isDigit := regexp.MustCompile(`\d`)
-		for _, c := range strings.Split(cardContent[0], "") {
+		// add space to verify last digit will be processed within the loop
+		for _, c := range strings.Split(cardContent[0]+" ", "") {
 			if c != " " && isDigit.MatchString(c) {
 				currentNumber += c
 			}
@@ -42,14 +43,10 @@ func main() {
 				currentNumber = ""
 			}
 		}
-		if currentNumber != "" {
-			num, _ := strconv.Atoi(currentNumber)
-			winningNumbers[num] = struct{}{}
-			currentNumber = ""
-		}
 		var sumCard int
 		var wonCards int
-		for _, c := range strings.Split(cardContent[1], "") {
+		// add space to verify last digit will be processed within the loop
+		for _, c := range strings.Split(cardContent[1]+" ", "") {
 			if c != " " && isDigit.MatchString(c) {
 				currentNumber += c
 			}
@@ -70,23 +67,6 @@ func main() {
 				}
 				currentNumber = ""
 			}
-		}
-		if currentNumber != "" {
-			num, _ := strconv.Atoi(currentNumber)
-			if _, ok := winningNumbers[num]; ok {
-				if sumCard == 0 {
-					sumCard++
-				} else {
-					sumCard *= 2
-				}
-				wonCards++
-				wonCard := i + wonCards
-				if multipliers[wonCard] == 0 {
-					multipliers[wonCard] = 1
-				}
-				multipliers[wonCard] += multipliers[i]
-			}
-			currentNumber = ""
 		}
 		sum += sumCard
 	}
